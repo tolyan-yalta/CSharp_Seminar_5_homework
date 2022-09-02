f26c943a548a911c95529cbb7630936b9c3ee7c8
anatoly <tolyan.yalta@yandex.ru>
2022-09-02 11:05:43 +0300

    Add Task_1. The number of even numbers in the array

---
+++ Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int random_number()
+{
+    int number = new Random().Next(100, 1000);
+    return number;
+}
+
+Console.Write("Задайте размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[length];
+int sum = 0;
+Console.Write("array[i]: ");
+for(int i = 0; i < length; i++)
+{
+    array[i] = random_number();
+    
+    Console.Write($" {array[i]}");
+    if(array[i] % 2 == 0)
+    sum = sum + 1;
+}
+
+Console.WriteLine("\n" + "В полученном массиве количество чётных чисел равно: " + sum);
