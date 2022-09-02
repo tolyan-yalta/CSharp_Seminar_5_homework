6d0edd4e3d695375cf5b10b4cdfec3d1eaeedfc1
anatoly <tolyan.yalta@yandex.ru>
2022-09-02 12:08:42 +0300

    Add Task_2. The sum of even elements standing in odd positions

---
+++ Task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму четных элементов, стоящих на нечётных позициях 
+// (позиция - индекс элемента в массиве).
+
+int random_number(int min, int max)
+{
+    int number = new Random().Next(min, max);
+    return number;
+}
+
+Console.WriteLine("Задайте минимальное и максимальное значение для генерации чисел: ");
+int min = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Задайте количество элементов в массиве: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[length];
+int sum = 0;
+Console.Write("array[i]: ");
+for(int i = 0; i < length; i++)
+{
+    array[i] = random_number(min, max);
+    Console.Write($"{array[i]} ");
+    if(i % 2 == 1)
+        if(array[i] % 2 == 0)   sum += array[i];
+}
+
+Console.WriteLine("\n" + "Сумма четных элементов, стоящих на нечётных позициях равна: " + sum);
